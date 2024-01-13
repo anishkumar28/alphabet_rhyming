2d614b9c08a32ac63973cf7acf406f7dd9be483a
Anish Kumar <anishkumar6527@gmail.com>
2024-01-13 20:32:04 +0530

    adding all buttons

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ function speak(letter){
     else if(letter == "k") audio.src="sound/kites.mp3";
     else if(letter == "l") audio.src="sound/lion.mp3";
     else if(letter == "m") audio.src="sound/monkey.mp3";
+    else if(letter == "n") audio.src="sound/nest.mp3";
+    else if(letter == "o") audio.src="sound/orange.mp3";
+    else if(letter == "p") audio.src="sound/pen.mp3";
+    else if(letter == "q") audio.src="sound/queen.mp3";
+    else if(letter == "r") audio.src="sound/rat.mp3";
+    else if(letter == "s") audio.src="sound/sun.mp3";
+    else if(letter == "t") audio.src="sound/tree.mp3";
+    else if(letter == "u") audio.src="sound/umbrella.mp3";
+    else if(letter == "v") audio.src="sound/voilin.mp3";
+    else if(letter == "w") audio.src="sound/watch.mp3";
+    else if(letter == "x") audio.src="sound/x-ray.mp3";
+    else if(letter == "y") audio.src="sound/yak.mp3";
+    else if(letter == "z") audio.src="sound/zebra.mp3";
     speaker.style.color = "black";
     audio.play();
     setTimeout(function(){
